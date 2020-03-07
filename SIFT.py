55f3ea0dfa2f65aab5df67aa94148f2a65d51822
WaiYanNyeinNaing <31321485+WaiYanNyeinNaing@users.noreply.github.com>
2020-03-07 17:11:03 +0800

    Add files via upload

---
+++ SIFT.py
@@ -0,0 +1,45 @@
+import numpy as np
+import cv2
+
+input_img = 'linze1.jpg'
+
+img1 = cv2.imread(input_img, 0)          # query Image
+img2 = cv2.imread('template.jpg',0)      # target Image
+
+#sift
+sift = cv2.xfeatures2d.SIFT_create()
+
+kp1, des1 = sift.detectAndCompute(img1,None)
+kp2, des2 = sift.detectAndCompute(img2,None)
+
+#feature matching
+bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
+
+# Match descriptors.
+matches = bf.match(des1,des2)
+
+# Sort them in the order of their distance.
+matches = sorted(matches, key = lambda x:x.distance)
+
+good_matches = matches[:10]
+
+src_pts = np.float32([ kp1[m.queryIdx].pt for m in good_matches     ]).reshape(-1,1,2)
+dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good_matches ]).reshape(-1,1,2)
+M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+matchesMask = mask.ravel().tolist()
+h,w = img1.shape[:2]
+pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+
+dst = cv2.perspectiveTransform(pts,M)
+dst += (w, 0)  # adding offset
+
+draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+               singlePointColor = None,
+               matchesMask = matchesMask, # draw only inliers
+               flags = 2)
+
+img3 = cv2.drawMatches(img1,kp1,img2,kp2,good_matches, None,**draw_params)
+
+cv2.imwrite(f'result_{input_img}.png',img3)
+cv2.imshow("result",img3)
+cv2.waitKey()
